ab5ee98a8c590811e85185c6de8e54954ebac98b
Jason Carcamo <jasoncarcamo30@yahoo.com>
2021-02-24 21:10:16 -0500

    Added order router and service

---
+++ src/routes/OrderRouters/OrdersRouter.js
@@ -0,0 +1,154 @@
+const express = require("express");
+const OrdersRouter =  express.Router();
+const OrderService = require("../../services/OrderService/OrderService");
+const {requireAuth} = require("../../middleware/jwtAuth");
+
+OrdersRouter
+    .route("/orders")
+    .all(requireAuth)
+    .get((req, res)=>{
+        const database = req.app.get("db");
+
+        OrderService.getAllOrders(database)
+            .then( orders => {
+                if(orders.length){
+                    return res.status(404).json({
+                        error: "There are no orders"
+                    });
+                };
+
+                return res.status(200).json({
+                    orders
+                });
+            });
+    })
+    .post((req, res)=>{
+        const database = req.app.get("db");
+        const {
+            order_type,
+            customer_first_name,
+            customer_last_name,
+            customer_mobile_number,
+            customer_address,
+            customer_city,
+            customer_state,
+            customer_zip_code,
+            subtotal,
+            time_placed,
+            time_ready,
+            date_createdL,
+            customer_id
+        } = req.body;
+        const newOrder = {
+            order_type,
+            customer_first_name,
+            customer_last_name,
+            customer_mobile_number,
+            customer_address,
+            customer_city,
+            customer_state,
+            customer_zip_code,
+            subtotal,
+            time_placed,
+            time_ready,
+            date_createdL,
+            customer_id
+        };
+
+        for(const [key, value] of Object.entries(newOrder)){
+            if(value === undefined || value === ""){
+                return res.status(400).json({
+                    error: `${key.split("_").join(" ")} can only have a value or be null`
+                });
+            };
+        };
+
+        OrderService.createOrder(database, newOrder)
+            .then( createdOrder => {
+                return res.status(200).json({
+                    createdOrder
+                });
+            });
+    });
+
+OrdersRouter
+    .route("/order/:id")
+    .all(requireAuth)
+    .get((req, res)=>{
+        const database = req.app.get("db");
+
+        
+    })
+    .patch((req, res)=>{
+        const database = req.app.get("db");
+        const id = req.params.id;
+        const {
+            order_type,
+            customer_first_name,
+            customer_last_name,
+            customer_mobile_number,
+            customer_address,
+            customer_city,
+            customer_state,
+            customer_zip_code,
+            subtotal,
+            time_placed,
+            time_ready,
+            date_createdL,
+            customer_id
+        } = req.body;
+        const updateOrder = {
+            order_type,
+            customer_first_name,
+            customer_last_name,
+            customer_mobile_number,
+            customer_address,
+            customer_city,
+            customer_state,
+            customer_zip_code,
+            subtotal,
+            time_placed,
+            time_ready,
+            date_createdL,
+            customer_id
+        };
+
+        OrderService.getOrderById(database, id)
+            .then( order => {
+                if(!order){
+                    return res.status(404).json({
+                        error: `Order not found`
+                    });
+                };
+
+                OrderService.updateOrder(database, updateOrder, id)
+                    .then( updatedOrder => {
+                        return res.status(200).json({
+                            updatedOrder
+                        });
+                    });
+            });
+    })
+    .delete((req, res)=>{
+        const database = req.app.get("db");
+        const id = req.params.id;
+
+        OrderService.getOrderById(database, id)
+            .then( order => {
+
+                if(!order){
+                    return res.status(404).json({
+                        error: `Order not found`
+                    });
+                };
+
+                OrderService.deleteOrder(database, id)
+                    .then( deletedOrder => {
+                        return res.status(200).json({
+                            deletedOrder: order
+                        });
+                    });
+            });
+    });
+
+module.exports = OrderService;
---
+++ src/services/OrderService/OrderService.js
@@ -0,0 +1,22 @@
+const OrderService = {
+    getAllOrders(db){
+        return db.select("*").from("orders");
+    },
+    getAllCustomerOrders(db, customer_id){
+        return db.select("*").from("orders").where({customer_id});
+    },
+    getOrderById(db, id){
+        return db.select("*").from("orders").where({id}).first();
+    },
+    createOrder(db, newOrder){
+        return db.insert(newOrder).into("orders").returning("*").then(([createdOrder]) => createdOrder);
+    },
+    updateOrder(db, updateOrder, id){
+        return db.update(updateOrder).from("orders").returning("*").then(([updatedOrder]) => updatedOrder);
+    },
+    deleteOrder(db, id){
+        return db.delete().from("orders").where({id}).returning("*").then(([deletedOrder]) => deletedOrder);
+    }
+};
+
+module.exports = OrderService;
