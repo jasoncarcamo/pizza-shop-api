b42c8c0eb6a6ffb8ec58ad8dc712bfd00742a88b
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-03-01 13:29:33 -0800

    QUick commit

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
-    JWT_SECRET: process.env.SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV
 };
