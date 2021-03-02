a9ff6636b7fcaa75972e4784ca8c1ac9b809f488
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-03-01 18:52:23 -0800

    Quick commit

--- src/app/server.js
+++ src/app/server.js
@@ -3,10 +3,7 @@ const knex = require("knex");
 const {PORT, DATABASE_URL} = require("../../config");
 const db = knex({
     client: "pg",
-    connection: DATABASE_URL,
-    ssl: { 
-        rejectUnauthorized: false 
-    } 
+    connection: DATABASE_URL
 });
 
 app.set("db", db);
