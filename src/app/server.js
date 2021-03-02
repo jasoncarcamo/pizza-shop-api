cb44ee7101d64b105d8210245786b571273c3d68
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-03-01 18:17:42 -0800

    updated pg

--- src/app/server.js
+++ src/app/server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const knex = require("knex");
 const {PORT, DATABASE_URL} = require("../../config");
-
 const db = knex({
     client: "pg",
     connection: DATABASE_URL
